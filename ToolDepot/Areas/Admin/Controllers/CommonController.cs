0a2cfd2bb9f12278c6f040b2e5c00c4f8a1a00b9
Zeeshan Raja <zeeshan@unigo.com>
2013-01-09 14:30:52 -0500

    updates

---
+++ ToolDepot/Areas/Admin/Controllers/CommonController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using ToolDepot.Core;
+using ToolDepot.Models.Common;
+
+namespace ToolDepot.Areas.Admin.Controllers
+{
+    public class CommonController : Controller
+    {
+        private readonly IWorkContext _workContext;
+        public CommonController(IWorkContext workContext)
+        {
+            _workContext = workContext;
+        }
+
+        [ChildActionOnly]
+        public ActionResult AdminHeader()
+        {
+            var model = new HeaderModel {IsLoggedIn = _workContext.IsLoggedIn};
+            return PartialView(model);
+        }
+        [ChildActionOnly]
+        public ActionResult AdminFooter()
+        {
+            return PartialView();
+        }
+
+    }
+}
